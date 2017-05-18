92986c8768fff629aef5a1225a47c839f56df2b9
alekscherny <aleks.cherny@mail.ru>
2017-05-18 17:35:34 +0300

    Create Main Activity and layout

---
+++ app/src/main/java/ru/a89uchet/remindme/MainActivity.java
@@ -0,0 +1,18 @@
+package ru.a89uchet.remindme;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by aleks on 18.05.17.
+ */
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
