720e2846963e9a3d166b0346813e46d644c6de3e
alekscherny <aleks.cherny@mail.ru>
2017-05-19 11:32:04 +0300

    Create Main Activity and layout

--- app/src/main/java/ru/a89uchet/remindme/MainActivity.java
+++ app/src/main/java/ru/a89uchet/remindme/MainActivity.java
@@ -3,17 +3,34 @@ package ru.a89uchet.remindme;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 /**
  * Created by aleks on 18.05.17.
  */
 
 public class MainActivity extends Activity {
+   private Toolbar toolbar;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initToolBar();
 
+    }
+
+    private void initToolBar() {
+        toolbar=(Toolbar)findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.app_name);
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                return false;
+            }
+        });
+
+        toolbar.inflateMenu(R.menu.menu);
     }
 }
